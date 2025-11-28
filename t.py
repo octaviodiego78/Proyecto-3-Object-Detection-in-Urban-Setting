4990381bfd77b45c4db3a6b032e794384f8b4b93
Octavio Perez <diegooctavioperez21@gmail.com>
2025-11-27 21:37:53 -0600

    Added files

--- t.py
+++
@@ -1,6 +0,0 @@
-from transformers import AutoImageProcessor, Mask2FormerForUniversalSegmentation
-print('Imported')
-processor = AutoImageProcessor.from_pretrained("facebook/mask2former-swin-large-mapillary-vistas-semantic")
-model = Mask2FormerForUniversalSegmentation.from_pretrained("facebook/mask2former-swin-large-mapillary-vistas-semantic")
-print('Model loaded')
-print('Hola')
